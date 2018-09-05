0797c4269f645b69609b2c0747b7f660316a90bd
See Loo Jane <muigek34@yahoo.com.sg>
2018-09-06 00:13:11 +0800

    remove unnecessary code in BiblioAppTest

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -6,40 +6,7 @@ import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-
 public class BibliotecaAppTest {
 	
-	private BibliotecaApp app;
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final PrintStream originalOut = System.out;
-	
-	@Before
-	public void init() {
-		app = new BibliotecaApp();
-		setupStreams();
-	}
-	
-	@After
-	public void teardown() {
-		app = null;
-		restoreStreams();
-	}
-	
-	private void restoreStreams() {
-	    System.setOut(originalOut);
-	}
-	
-	
-	private void setupStreams() {
-	    System.setOut(new PrintStream(outContent));
-	}
-	
-	@Test
-    public void test() {
-
-    }
 
 }
