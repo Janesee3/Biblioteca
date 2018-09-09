f0c458a661f249ddfb844a9dea2798370737df75
See Loo Jane <muigek34@yahoo.com.sg>
2018-09-09 21:12:27 +0800

    add implementation and test cases for parsing list book commands

--- src/com/twu/biblioteca/Parser.java
+++ src/com/twu/biblioteca/Parser.java
@@ -2,10 +2,16 @@ package com.twu.biblioteca;
 
 public class Parser {
 	
+	public static Integer PARSED_INVALID_ID = -1;
+	
 	public static Action parse(AppState state, String input) {
 		
 		if (state == AppState.MAIN_MENU) {
 			return Parser.parseMainMenuSelection(input);
+		}
+		
+		if (state == AppState.LIST_BOOKS) {
+			return Parser.parseListBookMenuSelection(input);
 		}
 		
 		return null;
@@ -15,12 +21,12 @@ public class Parser {
 	public static int getIdFromActionStatement(String statement) {
         String[] wordsArray = statement.split(" ");
         if (wordsArray.length <= 1) {
-            return -1;
+            return PARSED_INVALID_ID;
         }
         try {
             return Integer.parseInt(wordsArray[1]);
         } catch (Exception e) {
-            return -1;
+            return PARSED_INVALID_ID;
         }
     }
 	
@@ -36,8 +42,21 @@ public class Parser {
 		if (input.equals(UserInterface.MENU_CHOICE_RETURN_BOOKS)) {
 			return new Action(ActionType.GOTO_RETURN_BOOKS);
 		}
-		
 		return new Action(ActionType.INVALID_MENU_CHOICE);	
+	}
+	
+	private static Action parseListBookMenuSelection(String input) {
+		if (input.equals(UserInterface.BOOK_LIST_CHOICE_BACK)) {
+			return new Action(ActionType.BACK_TO_MAIN_MENU);
+		}
 		
+		if (input.contains(UserInterface.BOOK_LIST_CHOICE_CHECKOUT)) {
+			Integer id = Parser.getIdFromActionStatement(input);
+			Action action = new Action(ActionType.CHECKOUT_BOOK);
+			action.addArg(id);
+			return action;
+		}
+		
+		return new Action(ActionType.INVALID_LIST_BOOK_MENU_CHOICE);
 	}
 }
--- test/com/twu/biblioteca/ParserTest.java
+++ test/com/twu/biblioteca/ParserTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 public class ParserTest {
 	
+	// Test for parsing main menu state input
+	
 	@Test
 	public void parseListBookSelectionReturnsCorrectAction() {
 		String input = UserInterface.MENU_CHOICE_LIST_BOOKS;
@@ -23,7 +25,7 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void parseListQuitSelectionReturnsCorrectAction() {
+	public void parseQuitSelectionReturnsCorrectAction() {
 		String input = UserInterface.MENU_CHOICE_QUIT;
 		Action expectedAction = new Action(ActionType.QUIT);
 		Action action = Parser.parse(AppState.MAIN_MENU, input);
@@ -31,11 +33,53 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void parseListInvalidMenuSelectionReturnsCorrectAction() {
+	public void parseInvalidMenuSelectionReturnsCorrectAction() {
 		String input = "12312321";
 		Action expectedAction = new Action(ActionType.INVALID_MENU_CHOICE);
 		Action action = Parser.parse(AppState.MAIN_MENU, input);
 		assertEquals(expectedAction, action);
 	}
+	
+	// Test for parsing list book menu input
+	
+	@Test
+	public void parseBackFromListBookReturnsCorrectAction() {
+		String input = UserInterface.BOOK_LIST_CHOICE_BACK;
+		Action expectedAction = new Action(ActionType.BACK_TO_MAIN_MENU);
+		Action action = Parser.parse(AppState.LIST_BOOKS, input);
+		assertEquals(expectedAction, action);
+	}
+	
+	@Test
+	public void parseCheckoutBookReturnsCorrectActionWithArg() {
+		Integer bookId = 1;
+		String input = UserInterface.BOOK_LIST_CHOICE_CHECKOUT + " " + bookId;
+		Action expectedAction = new Action(ActionType.CHECKOUT_BOOK);
+		expectedAction.addArg(bookId);
+		
+		Action action = Parser.parse(AppState.LIST_BOOKS, input);
+
+		assertEquals(expectedAction, action);
+	}
+	
+	@Test
+	public void parseCheckoutInvalidBookReturnsCorrectActionWithArg() {
+		String input = UserInterface.BOOK_LIST_CHOICE_CHECKOUT + " " + "absdad";
+		Action expectedAction = new Action(ActionType.CHECKOUT_BOOK);
+		expectedAction.addArg(Parser.PARSED_INVALID_ID);
+		
+		Action action = Parser.parse(AppState.LIST_BOOKS, input);
+
+		assertEquals(expectedAction, action);
+	}
+	
+	@Test
+	public void parseListBookInvalidSelectionReturnsCorrectAction() {
+		String input = "asdsad";
+		Action expectedAction = new Action(ActionType.INVALID_LIST_BOOK_MENU_CHOICE);
+		Action action = Parser.parse(AppState.LIST_BOOKS, input);
+		assertEquals(expectedAction, action);
+	}
+	
 
 }
