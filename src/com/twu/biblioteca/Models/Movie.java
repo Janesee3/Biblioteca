c60279d792e28126771a2177894fe8e0895fa45f
See Loo Jane <muigek34@yahoo.com.sg>
2018-09-10 21:42:02 +0800

    create Movie model and test cases for it

---
+++ src/com/twu/biblioteca/Models/Movie.java
@@ -0,0 +1,54 @@
+package com.twu.biblioteca.Models;
+
+import com.twu.biblioteca.EnumTypes.Rating;
+
+public class Movie {
+
+    private int index;
+	private String name;
+	private String director;
+	private String year;
+	private Rating rating;
+	private boolean isCheckedOut;
+	
+	public Movie(int index, String name, String director, String year, Rating rating) {
+		this.index = index;
+		this.name = name;
+		this.director = director;
+		this.year = year;
+		this.rating = rating;
+		this.isCheckedOut = false;
+	}
+	
+	 public int getIndex() {
+ 		return this.index;
+	 }
+	 
+	 public String getName() {
+	     return this.name;
+	 }
+	
+	 public String getDirector() {
+	     return this.director;
+	 }
+	
+	 public String getYear() {
+	     return this.year;
+	 }
+	 
+	 public Rating getRating() {
+		 return this.rating;
+	 }
+	
+	 public boolean getCheckoutStatus() {
+	     return this.isCheckedOut;
+	 }
+	
+	 public void markAsCheckedOut() {
+	     this.isCheckedOut = true;
+	 }
+	
+	 public void markAsNotCheckedOut() {
+	     this.isCheckedOut = false;
+	 }
+}
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,49 @@
+package com.twu.biblioteca;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.twu.biblioteca.EnumTypes.Rating;
+import com.twu.biblioteca.Models.Movie;
+
+public class MovieTest {
+	
+	Movie movie;
+	
+	int index = 10;
+    String name = "Test name";
+    String director = "meow";
+    String year = "1995";
+    Rating rating = Rating.THREE;
+	
+	 @Before
+	    public void init() {
+	        movie = new Movie(index, name, director, year, rating);
+	    }
+
+	    @Test
+	    public void creatingMovieShouldCorrectlyPassAttributes() {
+		    	assertEquals(this.index, this.movie.getIndex());    
+		    	assertEquals(this.name, this.movie.getName());
+	        assertEquals(this.director, this.movie.getDirector());
+	        assertEquals(this.year, this.movie.getYear());
+	        assertEquals(this.rating, this.movie.getRating());
+	        assertEquals(false, this.movie.getCheckoutStatus());
+	    }
+
+	    @Test
+	    public void checkOutMovieShouldUpdateCorrectly() {
+	        this.movie.markAsCheckedOut();
+	        assertEquals(true, this.movie.getCheckoutStatus());
+	    }
+
+	    @Test
+	    public void unCheckOutMovieShouldUpdateCorrectly() {
+	        this.movie.markAsCheckedOut();
+	        this.movie.markAsNotCheckedOut();
+	        assertEquals(false, this.movie.getCheckoutStatus());
+	    }
+
+}
