e321dc33f2b7b5a2ceb0f7c48da34dd5feab435f
janesee3 <muigek34@yahoo.com.sg>
2018-09-11 11:00:42 +0800

    shorten if statement

--- src/com/twu/biblioteca/Parser.java
+++ src/com/twu/biblioteca/Parser.java
@@ -27,9 +27,7 @@ public class Parser {
 	// Example of action statement: "<action> <id>"
 	public static int getIdFromActionStatement(String statement) {
         String[] wordsArray = statement.split(" ");
-        if (wordsArray.length <= 1) {
-            return PARSED_INVALID_ID;
-        }
+        if (wordsArray.length <= 1) { return PARSED_INVALID_ID; }
         try {
             return Integer.parseInt(wordsArray[1]);
         } catch (Exception e) {
